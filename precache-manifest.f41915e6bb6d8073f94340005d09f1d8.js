4a37c096bc5a950f35ebb6af0d4e9a150b8417b9
Nikita Balikhin <balikhin.n.e@sberbank.ru>
2019-05-16 23:01:36 +0300

    Updates

---
+++ precache-manifest.f41915e6bb6d8073f94340005d09f1d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b2dcd93e904e7ce8579611a596f560e",
+    "url": "/regous_ui_kit/index.html"
+  },
+  {
+    "revision": "3a1a1d635dc65e5057ab",
+    "url": "/regous_ui_kit/static/css/main.5c01aaf5.chunk.css"
+  },
+  {
+    "revision": "3a992b6764601035953b",
+    "url": "/regous_ui_kit/static/js/2.a62319da.chunk.js"
+  },
+  {
+    "revision": "3a1a1d635dc65e5057ab",
+    "url": "/regous_ui_kit/static/js/main.59f74e8f.chunk.js"
+  },
+  {
+    "revision": "098ce0b0fcbf1fc7fc62",
+    "url": "/regous_ui_kit/static/js/runtime~main.efc0d6b2.js"
+  },
+  {
+    "revision": "055a94ca33515438fd5a1d2b98919c64",
+    "url": "/regous_ui_kit/static/media/close.055a94ca.svg"
+  }
+]);
